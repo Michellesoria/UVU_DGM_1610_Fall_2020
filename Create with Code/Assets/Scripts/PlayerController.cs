1537a99d9de888ecb13c49e80f90c27ff126b411
Michelle <michelle_soria1@hotmail.com>
2020-09-19 22:43:44 -0600

    Added Vehicle movement and turn functionality

--- Create with Code/Assets/Scripts/PlayerController.cs
+++ Create with Code/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed = 20;
+    public int speed = 2;
+    public float turnSpeed;
+
+    public float hInput;
+    public float fInput; 
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +20,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * fInput * speed);
+        transform.Rotate(Vector3.up * Time.deltaTime * hInput * turnSpeed);
     }
 }
