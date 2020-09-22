0dbbc29b899a616f8e9b6aabcbec7367487cc81b
Michelle <michelle_soria1@hotmail.com>
2020-09-21 20:17:36 -0600

    completed challenge 1

--- Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed;
-    public float verticalInput;
+    private float speed = 10.0f;
+    private float rotationSpeed;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
