d926c3dcdc1067301d6ad3646fda41288ff44c48
Michelle <michelle_soria1@hotmail.com>
2020-11-02 11:04:36 -0700

    Completed Jump and fall anim. Prevented post death jump

--- Prototype 3-Run and Jump.2/Assets/Scripts/PlayerController.cs
+++ Prototype 3-Run and Jump.2/Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true && !gameOver)
         {
             isGrounded = false; 
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -40,6 +40,8 @@ public class PlayerController : MonoBehaviour
             {
                 gameOver = true;
                 Debug.Log("Game Over!");
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
             }
         }
 }
