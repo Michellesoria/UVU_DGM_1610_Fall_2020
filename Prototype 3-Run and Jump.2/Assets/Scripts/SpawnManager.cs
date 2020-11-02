c36ef32b538ff174a581764d517d5683875d89af
Michelle <michelle_soria1@hotmail.com>
2020-11-02 10:09:10 -0700

    Created new prototype 3 in 2019 Unity so no bugs

---
+++ Prototype 3-Run and Jump.2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePreb;
+    private Vector3 spawnPos = new Vector3 (25, 0, 0);
+    private float startDelay = 1.5f;
+    private float repeatDelay = 1.5f;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacles", startDelay, repeatDelay);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    //If game over is false, continue to instantiate obstacles
+    void SpawnObstacles()
+    {
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePreb, spawnPos, obstaclePreb.transform.rotation);
+        }
+    }
+}
