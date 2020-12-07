9386ee64814f26441618c30c2cb6641c826ea57a
Michelle <michelle_soria1@hotmail.com>
2020-12-07 13:10:28 -0700

    imported dog, toys, & food, created forward, destroy and spawn scripts

---
+++ Play Time With Archie/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] toyPrefabs;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int toyIndex = Random.Range(0, toyPrefabs.Length);
+        Instantiate(toyPrefabs[toyIndex], new Vector3(0, 0, 13), toyPrefabs[toyIndex].transform.rotation);
+    }
+}
