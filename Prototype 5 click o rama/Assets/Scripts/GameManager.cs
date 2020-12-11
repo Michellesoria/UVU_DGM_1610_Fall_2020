9aaaa8eb28eedc4a57cfed62bd1701c315c7f8d7
Michelle <michelle_soria1@hotmail.com>
2020-12-10 21:03:41 -0700

    created gameover and resart button. game stops and starts

--- Prototype 5 click o rama/Assets/Scripts/GameManager.cs
+++ Prototype 5 click o rama/Assets/Scripts/GameManager.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive; 
     private int score;
     private float spawnRate = 1; 
     
@@ -14,8 +19,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
+        StartCoroutine(SpawnTarget());   
         UpdateScore(0);
     }
 
@@ -27,14 +33,13 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             //selects random object in the list
             int index = Random.Range(0, targets.Count);
             //spawns object
             Instantiate(targets[index]);
-         
         }
         
     }
@@ -43,5 +48,17 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "SCORE: " + score; 
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
